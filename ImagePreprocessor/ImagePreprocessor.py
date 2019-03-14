d8b83db4faaa90698ce6b43839fbbab4491d4389
Martin Horak <horczech@gmail.com>
2019-03-14 14:00:08 +0100

    Implement image preprocessing

--- ImagePreprocessor/ImagePreprocessor.py
+++ ImagePreprocessor/ImagePreprocessor.py
@@ -3,12 +3,12 @@ from abc import ABCMeta, abstractmethod
 from utilities.image import load_image
 
 
-
 class ImagePreprocessor(metaclass=ABCMeta):
-    def __init__(self, img_path):
+    def __init__(self, img_path, config):
         self.image = load_image(img_path, cv2.IMREAD_GRAYSCALE)
+
+        self.config = config
 
     @abstractmethod
     def do_preprocessing(self):
         pass
-
